cf142ffd7e1a4e77381e113490a246b15fba3189
Alex Erofeev <xnet141@gmail.com>
2024-09-23 08:17:09 +0300

    Add bookings link and page

--- app.rb
+++ app.rb
@@ -45,3 +45,18 @@ get '/barber/:id' do
 	@barber = Barber.find(params[:id])
 	erb :barber
 end
+
+get '/bookings' do
+	@clients = Client.order('created_at DESC')
+	erb :bookings
+end
+
+get '/client/:id' do
+	@client = Client.find(params[:id])
+	erb :client
+end
+
+
+
+
+
